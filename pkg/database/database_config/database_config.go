cc16c3befb6cb023d3cbd4f64db9432956cc8690
phn00dev <hudayberdipolat@gmail.com>
2025-01-09 10:11:44 +0500

    redis and database connection ready

--- pkg/database/database_config/database_config.go
+++ pkg/database/database_config/database_config.go
@@ -1,1 +1,36 @@
 package databaseconfig
+
+import (
+	"fmt"
+
+	"github.com/phn00dev/golang-news-app/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DatabaseConfig struct {
+	config *config.Config
+}
+
+func NewDbConfig(config *config.Config) *DatabaseConfig {
+	return &DatabaseConfig{
+		config: config,
+	}
+}
+
+func (dbConfig *DatabaseConfig) GetConfig() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		dbConfig.config.DatabaseConfig.Host,
+		dbConfig.config.DatabaseConfig.User,
+		dbConfig.config.DatabaseConfig.Password,
+		dbConfig.config.DatabaseConfig.Name,
+		dbConfig.config.DatabaseConfig.Port,
+		dbConfig.config.DatabaseConfig.SslMode,
+		dbConfig.config.DatabaseConfig.TimeZone,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ pkg/redisConfig/redis_config.go
@@ -0,0 +1,46 @@
+package redisconfig
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/phn00dev/golang-news-app/pkg/config"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisConfig struct {
+	config *config.Config
+	client *redis.Client
+}
+
+func NewRedisConfig(config *config.Config) *RedisConfig {
+	return &RedisConfig{
+		config: config,
+	}
+}
+
+func (redisConfig *RedisConfig) Connect() error {
+	host := redisConfig.config.RedisConfig.Host
+	port := redisConfig.config.RedisConfig.Port
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+
+	ctx := context.Background()
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		return fmt.Errorf("failed to connect to Redis: %w", err)
+	}
+
+	fmt.Println("Connected to Redis successfully!")
+	redisConfig.client = client
+	return nil
+}
+
+func (redisConfig *RedisConfig) GetClient() *redis.Client {
+	return redisConfig.client
+}
